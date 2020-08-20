1582dd99eb62bec2b21d95e2b8252e831b3981ec
ivanvinicius <ivanvinicius88@gmail.com>
2020-08-20 19:54:06 -0300

    feat: styling cart page

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,9 +1,130 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import tenis1Img from '../../assets/images/tenis1.jpg';
+
+import { Container, ProductTable, Total } from './styles';
 
 const Cart: React.FC = () => {
-  return <h1>cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>NOME</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+              <img src={tenis1Img} alt="Tênis" />
+            </td>
+            <td>
+              <strong>Tênis muito massa</strong>
+              <span>R$ 129,00</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline color="#7159c1" size={21} />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline color="#7159c1" size={21} />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete color="#7159c1" size={21} />
+              </button>
+            </td>
+          </tr>
+          {/*  */}
+          <tr>
+            <td>
+              <img src={tenis1Img} alt="Tênis" />
+            </td>
+            <td>
+              <strong>Tênis muito massa</strong>
+              <span>R$ 129,00</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline color="#7159c1" size={21} />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline color="#7159c1" size={21} />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete color="#7159c1" size={21} />
+              </button>
+            </td>
+          </tr>
+          {/*  */}
+          {/*  */}
+          <tr>
+            <td>
+              <img src={tenis1Img} alt="Tênis" />
+            </td>
+            <td>
+              <strong>Tênis muito massa</strong>
+              <span>R$ 129,00</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline color="#7159c1" size={21} />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline color="#7159c1" size={21} />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete color="#7159c1" size={21} />
+              </button>
+            </td>
+          </tr>
+          {/*  */}
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 };
 
 export default Cart;
---
+++ src/pages/Cart/styles.ts
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  padding: 30px;
+  background: var(--color-background-white);
+  border-radius: 4px;
+
+  footer {
+    margin-top: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    button {
+      display: flex;
+      align-items: center;
+      padding: 12px 20px;
+
+      border: 0;
+      border-radius: 4px;
+      background: var(--color-button);
+      color: var(--color-text-white);
+      font-weight: bold;
+      text-transform: uppercase;
+      overflow: hidden;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#7159c1')};
+      }
+    }
+  }
+`;
+
+export const ProductTable = styled.table`
+  width: 100%;
+
+  thead th {
+    color: var(--color-text-gray);
+    text-align: left;
+    padding: 12px;
+  }
+
+  tbody td {
+    padding: 12px;
+    vertical-align: middle;
+    border-bottom: 1px solid #ddd;
+  }
+
+  img {
+    height: 100px;
+  }
+
+  strong {
+    color: var(--color-text-gray);
+    display: block;
+  }
+
+  span {
+    display: block;
+    margin-top: 5px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    input {
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      color: #666;
+      padding: 6px;
+      width: 50px;
+      text-align: end;
+    }
+  }
+
+  button {
+    background: none;
+    border: 0;
+    padding: 6px;
+  }
+`;
+
+export const Total = styled.div`
+  display: flex;
+  align-items: baseline;
+
+  span {
+    color: #999;
+    font-weight: bold;
+    margin-right: 10px;
+  }
+
+  strong {
+    font-size: 28px;
+    margin-left: 5px;
+  }
+`;
