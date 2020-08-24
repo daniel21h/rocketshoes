949dc2be1ffd5ecc7498704ce8653b8c1f4c6f92
ivanvinicius <ivanvinicius88@gmail.com>
2020-08-24 18:30:36 -0300

    feat: separating actions

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,16 +1,34 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
+import { formatPrice } from '../../../utils/format';
 import api from '../../../services/api';
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
+import IProductData from '../../../dtos/IProductData';
 
 interface IAddToCartData {
   id: number;
 }
 
 function* addToCart({ id }: IAddToCartData) {
-  const response = yield call(api.get, `/products/${id}`);
+  const productExist = yield select((state) =>
+    state.cart.find((p: IProductData) => p.id === id),
+  );
 
-  yield put(addToCartSuccess(response.data));
+  if (productExist) {
+    const amount = productExist.amount + 1;
+
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest<string, any>('@cart/ADD_REQUEST', addToCart)]);
